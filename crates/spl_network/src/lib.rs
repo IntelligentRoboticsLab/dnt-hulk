f9de937b66cddc3dad41b493e6412cd126fef2be
Fyor Klein Gunnewiek <fyorkg@gmail.com>
2023-04-04 23:57:21 +0200

    Refactor of conversion.rs file.

--- crates/spl_network/src/lib.rs
+++ crates/spl_network/src/lib.rs
@@ -1,6 +1,6 @@
 mod bindings;
-mod game_controller_conversion;
 mod game_controller_return_message;
+mod game_controller_state_conversion;
 mod game_controller_state_message;
 mod spl_message;
 
@@ -9,10 +9,10 @@ use std::time::Duration;
 use nalgebra::Point2;
 use serde::{Deserialize, Serialize};
 
-pub use game_controller_conversion::{
+pub use game_controller_return_message::GameControllerReturnMessage;
+pub use game_controller_state_conversion::{
     GamePhase, GameState, Half, Penalty, PenaltyShoot, Player, SetPlay, Team, TeamColor, TeamState,
 };
-pub use game_controller_return_message::GameControllerReturnMessage;
 pub use game_controller_state_message::GameControllerStateMessage;
 pub use spl_message::SplMessage;
 
