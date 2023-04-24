098d583bef8ad62b86851115f92cd83d18de5665
Mark Honkoop <112875887+MarkHonkoop@users.noreply.github.com>
2023-04-24 22:19:35 +0200

    Should no longer wee if fallen/falling

--- crates/control/src/wee_sound.rs
+++ crates/control/src/wee_sound.rs
@@ -1,7 +1,7 @@
 use context_attribute::context;
 use std::time::Duration;
 
-use types::{CycleTime, SensorData, SolePressure};
+use types::{CycleTime, FallState, SensorData, SolePressure};
 
 use kira::{
     manager::{backend::cpal::CpalBackend, AudioManager, AudioManagerSettings},
@@ -27,7 +27,7 @@ pub struct CycleContext {
     pub sensor_data: Input<SensorData, "sensor_data">,
     pub sole_pressure: Input<SolePressure, "sole_pressure">,
     pub cycle_time: Input<CycleTime, "cycle_time">,
-
+    pub fall_state: Input<FallState, "fall_state">,
     pub hysteresis: Parameter<f32, "wee_sound.hysteresis">,
     pub pressure_threshold: Parameter<f32, "wee_sound.pressure_threshold">,
     pub timeout: Parameter<Duration, "wee_sound.timeout">,
@@ -48,7 +48,10 @@ impl PlaySound {
     }
 
     pub fn cycle(&mut self, context: CycleContext) -> color_eyre::Result<MainOutputs> {
-        if !context.has_ground_contact && !self.sound_played {
+        if !context.has_ground_contact
+            && *context.fall_state == FallState::Upright
+            && !self.sound_played
+        {
             let sound_data =
                 StaticSoundData::from_file("/etc/sounds/weeeee.wav", StaticSoundSettings::new())
                     .unwrap();
