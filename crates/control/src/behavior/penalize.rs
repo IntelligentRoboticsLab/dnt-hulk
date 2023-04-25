864858d5f0595e929ad064ba11a05efad847b93c
Fyor Klein Gunnewiek <fyorkg@gmail.com>
2023-04-25 07:25:53 +0200

    Comment code that should be removed.

--- crates/control/src/behavior/penalize.rs
+++ crates/control/src/behavior/penalize.rs
@@ -3,16 +3,16 @@ use types::{HeadMotion, MotionCommand, PrimaryState, WorldState};
 
 pub fn execute(world_state: &WorldState) -> Option<MotionCommand> {
     match world_state.robot.primary_state {
-        PrimaryState::Penalized => {
-            println!("This reaches Initial state.");
-            let robot_to_field = world_state.robot.robot_to_field?;
-            Some(MotionCommand::Stand {
-                head: HeadMotion::LookAt {
-                    target: robot_to_field.inverse() * Point2::origin(),
-                },
-            })
-        }
-        PrimaryState::Penalized | PrimaryState::Initial => Some(MotionCommand::Penalized),
+        // PrimaryState::Penalized => {
+        //     println!("This reaches Initial state.");
+        //     let robot_to_field = world_state.robot.robot_to_field?;
+        //     Some(MotionCommand::Stand {
+        //         head: HeadMotion::LookAt {
+        //             target: robot_to_field.inverse() * Point2::origin(),
+        //         },
+        //     })
+        // }
+        // PrimaryState::Penalized | PrimaryState::Initial => Some(MotionCommand::Penalized),
         _ => None,
     }
 }
