bd1873adce690b2f4bd32ec399b27cac5e857edb
Fyor Klein Gunnewiek <fyorkg@gmail.com>
2023-05-13 23:17:17 +0200

    Set the head angle target to middle circle in the new `Initial` state action

--- crates/control/src/behavior/initial.rs
+++ crates/control/src/behavior/initial.rs
@@ -1,11 +1,20 @@
+use nalgebra::Point2;
 use types::{HeadMotion, MotionCommand, PrimaryState, WorldState};
 
 pub fn execute(world_state: &WorldState) -> Option<MotionCommand> {
     match world_state.robot.primary_state {
-        PrimaryState::Initial => Some(MotionCommand::Stand {
-            head: HeadMotion::ZeroAngles,
-            is_energy_saving: true,
-        }),
+        PrimaryState::Initial => {
+            let robot_to_field = world_state
+                .robot
+                .robot_to_field
+                .expect("Failed to get robot_to_field.");
+            Some(MotionCommand::Stand {
+                head: HeadMotion::LookAt {
+                    target: robot_to_field.inverse() * Point2::origin(),
+                },
+                is_energy_saving: true,
+            })
+        }
         _ => None,
     }
 }
--- crates/control/src/behavior/stand.rs
+++ crates/control/src/behavior/stand.rs
@@ -8,12 +8,10 @@ pub fn execute(
     world_state: &WorldState,
     field_dimensions: &FieldDimensions,
 ) -> Option<MotionCommand> {
+    println!("robot_to_field");
+    let robot_to_field = world_state.robot.robot_to_field.unwrap();
     match world_state.robot.primary_state {
         PrimaryState::Initial | PrimaryState::Set => {
-            let robot_to_field = world_state
-                .robot
-                .robot_to_field
-                .expect("Failed to get robot_to_field.");
             let fallback_target = match world_state.game_controller_state {
                 Some(GameControllerState {
                     sub_state: Some(SubState::PenaltyKick),
