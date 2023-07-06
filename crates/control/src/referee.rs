f358da5bf55d5021986d402313d1818775d9de4b
lex <lex.bolt@student.uva.nl>
2023-07-06 12:53:39 +0200

    random number generated referee response

--- crates/control/src/referee.rs
+++ crates/control/src/referee.rs
@@ -1,19 +1,13 @@
-use std::time::{SystemTime};
 use color_eyre::{eyre::WrapErr, Result};
 use context_attribute::context;
-use compiled_nn::CompiledNN;
 use nalgebra::Isometry2;
+use rand::Rng;
 use spl_network_messages::{GameControllerReturnMessage, PlayerNumber};
-use types::{
-    hardware::Interface,
-    messages::{OutgoingMessage},
-    CycleTime, FilteredWhistle, ycbcr422_image::YCbCr422Image
-};
-use image::{RgbImage, ImageBuffer, Rgb, imageops};
+use std::time::SystemTime;
+use types::{hardware::Interface, messages::OutgoingMessage, CycleTime, FilteredWhistle};
 
 pub struct Referee {
     last_heard_timestamp: Option<SystemTime>,
-    lstm: CompiledNN
 }
 
 #[context]
@@ -28,17 +22,12 @@ pub struct CycleContext {
     pub cycle_time: Input<CycleTime, "cycle_time">,
     pub player_number: Parameter<PlayerNumber, "player_number">,
     pub robot_to_field: Input<Option<Isometry2<f32>>, "robot_to_field?">,
-    // pub image: Input<YCbCr422Image, "image">,
 }
 
 impl Referee {
     pub fn new(_context: CreationContext) -> Result<Self> {
-        let mut network = CompiledNN::default();
-        network.compile("tools/machine-learning/referee_challange/conv_orig_aug.h5");
-
         Ok(Self {
             last_heard_timestamp: None,
-            lstm: network,
         })
     }
 
@@ -48,7 +37,9 @@ impl Referee {
                 match cycle_time.duration_since(cycle_time) {
                     Ok(duration) => {
                         if duration.as_secs() < 20 {
-                            self.send_referee_message(&context, 1)?;
+                            let mut rng_gen = rand::thread_rng();
+                            let handsignal: u8 = rng_gen.gen_range(1..=16);
+                            self.send_referee_message(&context, handsignal)?;
                         }
                     }
                     Err(_err) => {}
@@ -56,56 +47,8 @@ impl Referee {
             }
         }
 
-        // let input_img = resize_image(&context.image);
-        // let input = self.lstm.classifier.input_mut(0);
-
-        // self.lstm.classifier.apply();
-        // self.lstm.classifier.output(0).data[0];
-
         Ok(())
     }
-
-    // fn rgb_image_from_buffer_422(width_422: u32, height: u32, buffer: &[YCbCr422]) -> RgbImage {
-    //     let mut rgb_image = RgbImage::new(2 * width_422, height);
-
-    //     for y in 0..height {
-    //         for x in 0..width_422 {
-    //             let pixel = buffer[(y * width_422 + x) as usize];
-    //             let left_color: Rgb = YCbCr444 {
-    //                 y: pixel.y1,
-    //                 cb: pixel.cb,
-    //                 cr: pixel.cr,
-    //             }
-    //             .into();
-    //             let right_color: Rgb = YCbCr444 {
-    //                 y: pixel.y2,
-    //                 cb: pixel.cb,
-    //                 cr: pixel.cr,
-    //             }
-    //             .into();
-    //             rgb_image.put_pixel(
-    //                 x * 2,
-    //                 y,
-    //                 image::Rgb([left_color.r, left_color.g, left_color.b]),
-    //             );
-    //             rgb_image.put_pixel(
-    //                 x * 2 + 1,
-    //                 y,
-    //                 image::Rgb([right_color.r, right_color.g, right_color.b]),
-    //             );
-    //         }
-    //     }
-
-    //     rgb_image
-    // }
-
-    // fn resize_image(src: &YCbCr422Image) -> Result<YCbCr422Image> {
-    //     let mut rgb:RgbImage = rgb_image_from_buffer_422(src.width() / 2, src.height());
-
-    //     let resized_image = imageops::resize(&rgb, 256, 256, image::imageops::FilterType::Triangle);
-    //     resized_image.to_image();
-    //     YCbCr422Image::from_raw_buffer(256 / 2, 256,resized_image.as_raw());
-    // }
 
     fn send_referee_message(
         &mut self,
